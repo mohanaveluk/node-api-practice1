2311ef782a7f267731a4769fdd8e33472b1b6108
Mohanavelu <mohanaveluk@gmail.com>
2021-09-02 10:11:38 -0500

    update index. config

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ let options = {
                 name: 'Mohanavelu Kumarsamy'
             }
         },
-        host: IN_PRODUCITION ? 'localhost:3000' : 'localhost:3000',
-        //host: IN_PRODUCITION ? 'palmofashion-hqhsvwro4a-uc.a.run.app' : 'localhost:3000',
+        //host: IN_PRODUCITION ? 'localhost:3000' : 'localhost:3000',
+        host: IN_PRODUCITION ? 'palmofashion-hqhsvwro4a-uc.a.run.app' : 'localhost:3000',
         basePath: '/',
         produces: [
             "application/json",
             "application/xml"
         ],
-        schemes: IN_PRODUCITION ? ['http', 'https'] : ['http', 'https'],
+        schemes: IN_PRODUCITION ? ['https', 'https'] : ['http', 'https'],
         securityDefinitions: {
             JWT: {
                 type: 'apiKey',
